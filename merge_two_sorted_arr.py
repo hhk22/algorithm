38d2719240220ccb72591c83dd7025f74ea29f65
Hyeonghwan <khhh9401@pioncorp.com>
2023-06-12 10:22:32 +0900

    leetcode prob4. merge two sorted array

---
+++ merge_two_sorted_arr.py
@@ -0,0 +1,20 @@
+
+from typing import List
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        combined_arr = sorted(nums1 + nums2)
+
+        mid = len(combined_arr) // 2
+        if len(combined_arr) % 2 != 0:
+            return float(combined_arr[mid])
+        else:
+            return (combined_arr[mid-1]+combined_arr[mid])/2
+
+
+s = Solution()
+nums1 = [1, 3]
+nums2 = [2]
+target = 2
+assert target == s.findMedianSortedArrays(nums1, nums2)
