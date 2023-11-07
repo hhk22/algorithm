742e78005f5aab30c1c0c2f4d795232eb40867cb
hhk22 <khhh9401@gmail.com>
2023-11-07 10:07:42 +0900

    jump_game_2

--- jump_game_2.py
+++ jump_game_2.py
@@ -1,12 +1,20 @@
-class Solution:
-    def jump(self, nums):
-        reach, count, last = 0, 0, 0
 
-        for i in range(len(nums)-1):
-            reach = max(reach, i + nums[i])
 
-            if i == last:
-                last = reach
-                count += 1
+nums = [2, 1, 1, 1, 1]
+output = 3
 
-        return count
+count = 0
+idx = 0
+right = 0
+reach = 0
+while idx < len(nums) - 1:
+    reach = max(reach, idx + nums[idx])
+
+    if idx == right:
+        right = reach
+        count += 1
+
+    idx = idx + 1
+
+print(count)
+
