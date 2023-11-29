62a02430f2e08b89014e232eac687d4ff23c0315
hhk22 <khhh9401@gmail.com>
2023-11-29 10:52:06 +0900

    insert intervals

--- insert_intervals.py
+++ insert_intervals.py
@@ -6,44 +6,21 @@ output = [[2, 7], [8, 10], [11, 13]]
 output = []
 
 
-def is_overlap(itv1, itv2):
-    if itv1[1] > itv2[1]:
-        if itv1[0] > itv2[1]:
-            return False
+def insert(intervals, newInterval):
+    result = []
+    for i in range(len(intervals)):
+        if newInterval[1] < intervals[i][0]:
+            result.append(newInterval)
+            return result + intervals[i:]
+        elif newInterval[0] > intervals[i][1]:
+            result.append(intervals[i])
         else:
-            return True
-    else:
-        if itv2[0] > itv1[1]:
-            return False
-        else:
-            return True
+            newInterval = [
+                min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])
+            ]
+    result.append(newInterval)
+    return result
 
 
-triggered = False
-finish = False
-while (intervals):
-    interval = intervals.pop(0)
-    if is_overlap(newInterval, interval):
-        newInterval[0] = min(newInterval[0], interval[0])
-        newInterval[1] = max(newInterval[1], interval[1])
-        triggered = True
-    else:
-        if triggered:
-            output.append(interval)
-            finish = True
-            break
-
-if finish is False:
-    if output and newInterval[1] < output[0][0]:
-        output = [newInterval] + output
-    
-
-    
-
-if finish is False:
-    if output and newInterval[1] < output[0][0]:
-        output = [newInterval] + output
-    else:
-        output.append(newInterval)
-
-print(output)
+rst = insert(intervals, newInterval)
+print(rst)
