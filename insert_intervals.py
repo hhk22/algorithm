b9412ab44bcb21cd7774c6c67366fcc3ed7adfe4
hhk22 <khhh9401@gmail.com>
2023-12-18 10:00:01 +0900

    insert intervals update

--- insert_intervals.py
+++ insert_intervals.py
@@ -8,15 +8,17 @@ output = []
 
 def insert(intervals, newInterval):
     result = []
+
     for i in range(len(intervals)):
-        if newInterval[1] < intervals[i][0]:
+        if newInterval[0] > intervals[i][1]:
+            result.append(intervals[i])
+        elif newInterval[1] < intervals[i][0]:
             result.append(newInterval)
             return result + intervals[i:]
-        elif newInterval[0] > intervals[i][1]:
-            result.append(intervals[i])
         else:
             newInterval = [
-                min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])
+                min(intervals[i][0], newInterval[0]),
+                max(intervals[i][1], newInterval[1])
             ]
     result.append(newInterval)
     return result
